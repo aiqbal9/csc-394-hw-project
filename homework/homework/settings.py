771f82f308f7f5b4408fe105f8cb2c4eda8d0984
abdul iqbal <aiqbal9@depaul.edu>
2023-04-19 21:15:05 +0500

    Homework 10

--- homework/homework/settings.py
+++ homework/homework/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'jawad021',
+        'PASSWORD': 'iqbal321',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
